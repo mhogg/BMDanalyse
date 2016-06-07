fb8654019e711a7f14e692c99b12fceb947efc35
Michael Hogg <michael.christopher.hogg@gmail.com>
2016-06-07 10:38:12 +1000

    Updating for v0.2.0 release

--- BMDanalyse/BMDanalyse.py
+++ BMDanalyse/BMDanalyse.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2013 Michael Hogg
+# Copyright (C) 2016 Michael Hogg
 
 # This file is part of BMDanalyse - See LICENSE.txt for information on usage and redistribution
 
